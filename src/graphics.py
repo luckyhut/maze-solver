bae3af7939b3d59daf84f204c03a82e8ffdb55e9
luckyhut <ed7johnson@gmail.com>
2025-02-01 14:28:30 -0700

    Added Point and Line and an example line in main()

--- src/graphics.py
+++ src/graphics.py
@@ -22,3 +22,23 @@ class Window:
 
     def close(self):
         self.running = False
+
+    def draw_line(self, line, fill_color):
+        line.draw(self.canvas, fill_color)
+
+class Line:
+    def __init__(self, x_one, y_one, x_two, y_two):
+        self.x1 = x_one
+        self.y1 = y_one
+        self.x2 = x_two
+        self.y2 = y_two
+
+    def draw(self, canvas, fill_color):
+        canvas.create_line(
+            self.x1, self.y1, self.x2, self.y2, fill=fill_color, width = 2
+        )
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
-from graphics import Window
+from graphics import Window, Line
 
 def main():
     win = Window(800, 600)
+    line = Line(0, 0, 400, 400)
+    win.draw_line(line, "red")
     win.wait_for_close()
 
 main()
